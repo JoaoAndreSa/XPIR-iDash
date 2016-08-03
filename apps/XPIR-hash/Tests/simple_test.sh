65dce29a05b2e3c97c48b846ecac32689cd38593
Carlos Aguilar <carlosaguilarmelchor@users.noreply.github.com>
2016-08-03 19:15:58 +0200

    Revert background server

--- apps/XPIR-hash/Tests/simple_test.sh
+++ apps/XPIR-hash/Tests/simple_test.sh
@@ -21,7 +21,7 @@ rm -rf data/*
 rm -rf output.txt
 
 #START SERVER
-lxterminal --working-directory=../PIRServer/ -e "./server"&
+lxterminal --working-directory=../PIRServer/ -e "./server"
 sleep 1
 
 #START CLIENT
