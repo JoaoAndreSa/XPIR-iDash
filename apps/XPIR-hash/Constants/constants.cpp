8da7bc6fb0bad4346dc5e414ab96978a634e61ae
Joao Andre Sa <jagsousa1992@gmail.com>
2016-12-12 06:41:31 -0800

    added test for 100,000

--- apps/XPIR-hash/Constants/constants.cpp
+++ apps/XPIR-hash/Constants/constants.cpp
@@ -13,15 +13,15 @@ namespace Constants{
 
     //client+server
     extern const char hostname[]{"localhost"};
-    extern const int port(12346);
+    extern const int port(12345);
     extern const double bandwith_limit(10000000);   //=0bps (means no bandwith limit)
     //extern const double bandwith_limit(0);
     extern const bool pipeline(true);               //=false (sequential execution);    =true (pipeline execution)
     extern const bool encrypted(true);              //=true (DB is symmetrically encrypted); =false (DB is in plaintext)
     extern const bool pre_import(true);
-    extern const uint64_t num_entries(8192);
+    extern const uint64_t num_entries(512);
     extern const int data_hash_size(48);
-    extern const int padding_size(736);
+    extern const int padding_size(256);
 
     //server
     extern const int n_threads(10);
